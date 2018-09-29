7ed1098da1def06a507eb0d2e083d59a1c82bfad
teleshik <ts.ts.smp@gmail.com>
2018-09-29 17:47:31 +0300

    working on slider

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,8 +1,6 @@
 $(function() {
   mobileNav();
-});
-
-
+}); 
 
 function mobileNav() {
   $('.home-menu-button').on('click', function(){
@@ -10,4 +8,22 @@ function mobileNav() {
     if(status){ $('.home-menu-button, .mobile-nav-home').removeClass('is-open'); }
     else { $('.home-menu-button, .mobile-nav-home').addClass('is-open'); }
   });
+}
+
+
+//Pure JS
+//Dot slider
+let slideIndex = 1
+
+let showSlides = x => {
+  let slides = document.getElementsByClassName("slide")
+  let dots = document.getElementsByClassName("dot")
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"
+  }
+  for (let i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace("active", "")
+  }
+  slides[slideIndex-1].style.display = "block"
+  dots[slideIndex-1].className += " active"
 }
